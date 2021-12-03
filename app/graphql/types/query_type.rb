0a1a91f1f6c5333ba24db45501f8998f372cc983
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-12-03 15:09:29 +0800

    Added Item & User Types & Objects

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :items,
+          [Types::ItemType],
+          null: false,
+          description: "Returns a list of items in the powerLib library."
+
+    def items
+      Item.all
+    end
+  end
+end
