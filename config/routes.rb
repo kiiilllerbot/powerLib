93fa7cebd31fa5cf7e89fefcb393f3130ed693a4
Shimol-Khan <shimolkhan.dev@gmail.com>
2021-12-03 15:26:26 +0800

    Config Apollo Client, GraphQL, React To List Items

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
   post "/graphql", to: "graphql#execute"
+
+  root 'library#index'
 end
